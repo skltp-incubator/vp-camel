23363e37262a6f8bbe86dda1f2af65ca88689e89
mtuliakova <mariia.tuliakova@soprasteria.com>
2018-07-03 17:07:34 +0200

    HSA cache instansieras nu när Camel Context startas

---
+++ vp-services-camel/src/main/java/se/skl/tp/vp/ContextListener.java
@@ -0,0 +1,33 @@
+package se.skl.tp.vp;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.StartupListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import se.skl.tp.hsa.cache.HsaCache;
+
+import static se.skl.tp.vp.constants.ApplicationProperties.HSA_FILES;
+
+public class ContextListener implements StartupListener {
+    private final Environment env;
+    private final HsaCache hsaCache;
+
+    public ContextListener(@Autowired Environment env, @Autowired HsaCache hsaCache) {
+        this.env = env;
+        this.hsaCache = hsaCache;
+    }
+
+    @Override
+    public void onCamelContextStarted(CamelContext camelContext, boolean b) throws Exception {
+        initHSACache();
+        initTakCache();
+    }
+
+    private void initHSACache() {
+        String[] hsaFiles = env.getProperty(HSA_FILES).split(",");
+        hsaCache.init(hsaFiles);
+    }
+    private void initTakCache() {
+
+    }
+}
