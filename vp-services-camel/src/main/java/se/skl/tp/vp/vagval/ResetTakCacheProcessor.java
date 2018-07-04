405ff6bc3b65e67ff410d38cc9de482cd356c4e8
mtuliakova <mariia.tuliakova@soprasteria.com>
2018-07-04 13:18:44 +0200

    ResetTakCache förbättringar

--- vp-services-camel/src/main/java/se/skl/tp/vp/vagval/ResetTakCacheProcessor.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/vagval/ResetTakCacheProcessor.java
@@ -6,20 +6,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import se.skltp.tak.vagval.wsdl.v2.ResetVagvalCacheResponse;
 import se.skltp.takcache.TakCache;
 import se.skltp.takcache.TakCacheLog;
 
+import java.util.Arrays;
+
 @Component
 public class ResetTakCacheProcessor implements Processor {
-    private Logger log = LoggerFactory.getLogger(ResetHsaCacheProcessor.class);
+    private Logger log = LoggerFactory.getLogger(ResetTakCacheProcessor.class);
+
+    private final TakCache takCache;
 
     @Autowired
-    private TakCache takCache;
-
+    public ResetTakCacheProcessor(TakCache takCache) {
+        this.takCache = takCache;
+    }
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        log.info("Start a reset of TAKcache.");
         TakCacheLog result = takCache.refresh();
-        exchange.getOut().setBody(result.isRefreshSuccessful());
+        exchange.getOut().setBody(getResultAsString(result));
+        exchange.getOut().setHeader("Content-Type", "text/html;");
+        exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 200);
+    }
+
+    private String getResultAsString(TakCacheLog result) {
+        StringBuilder resultAsString = new StringBuilder();
+        for (String processingLog : result.getLog()) {
+            resultAsString.append("<br>").append(processingLog);
+        }
+        return resultAsString.toString();
     }
 }
