c80218d862a0bf7bb22c8974b06fbed3e90118f6
hbragman <hans.bragman@soprasteria.com>
2019-04-08 14:40:02 +0200

    Two static strings.

--- vp-services-camel/src/main/java/se/skl/tp/vp/constants/VPExchangeProperties.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/constants/VPExchangeProperties.java
@@ -6,7 +6,8 @@ public class VPExchangeProperties {
     public static final String SENDER_ID = "senderid";
     public static final String SENDER_IP_ADRESS = "senderIpAdress";
     public static final String ORIGINAL_SERVICE_CONSUMER_HSA_ID = "originalServiceconsumerHsaid";
-
+    public static final String SKLTP_CORRELATION_ID = "skltp_correlationId";
+    public static final String IS_HTTPS = "is_https";
     public static final String RECEIVER_ID = "receiverid";
     public static final String RIV_VERSION = "rivversion";
     public static final String RIV_VERSION_OUT = "rivversion_out";
