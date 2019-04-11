d4b86e83fe185dccef5716d6588b9d67aa66f44e
hbragman <hans.bragman@soprasteria.com>
2019-04-11 09:30:27 +0200

    Reading propagateCorrelationIdForHttps from properties, plus other small changes.

--- vp-services-camel/src/main/java/se/skl/tp/vp/httpheader/HeaderConfigurationProcessorImpl.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/httpheader/HeaderConfigurationProcessorImpl.java
@@ -4,35 +4,46 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import se.skl.tp.vp.constants.HttpHeaders;
+import se.skl.tp.vp.constants.PropertyConstants;
 import se.skl.tp.vp.constants.VPExchangeProperties;
-
+import javax.annotation.PostConstruct;
 import java.util.UUID;
 
-@Service
-public class HeaderConfigurationProcessorImpl implements HeaderConfigurationProcessor{
 
+@Service
+public class HeaderConfigurationProcessorImpl implements HeaderConfigurationProcessor {
+
+    @Autowired
+    Environment env;
+    private static boolean propagateCorrelationIdForHttps;
     private static Logger LOGGER = LogManager.getLogger(HttpSenderIdExtractorProcessorImpl.class);
-    private boolean propagateCorrelationIdForHttps;
+
+    @PostConstruct
+    public void init() {
+        String s = env.getProperty(PropertyConstants.PROPAGATE_CORRELATION_ID_FOR_HTTPS);
+        propagateCorrelationIdForHttps = s.trim().equals("true") || s.equals("1");
+    }
 
     @Override
-    public void process(Exchange exchange) throws Exception {
+    public void process(Exchange exchange) {
         setOriginalConsumerId(exchange);
-        propagateCorrelationIdToProducer(exchange, propagateCorrelationIdForHttps);
+        propagateCorrelationIdToProducer(exchange);
     }
 
-    public void setPropagateCorrelationIdForHttps(final Boolean propagateCorrelationIdForHttps) {
-        this.propagateCorrelationIdForHttps = propagateCorrelationIdForHttps;
-    }
+    private void propagateCorrelationIdToProducer(Exchange exchange) {
 
-    private void propagateCorrelationIdToProducer(Exchange exchange, Boolean propagateCorrelationIdForHttps) {
         Message message = exchange.getIn();
-        String correlationId = message.getHeader(VPExchangeProperties.SKLTP_CORRELATION_ID, String.class);
+        String correlationId = message.getHeader(HttpHeaders.X_SKLTP_CORRELATION_ID, String.class);
         if (correlationId == null || correlationId.trim().isEmpty()) {
-            LOGGER.debug("Correlation id not found in http header. Will create a new one!");
             correlationId = UUID.randomUUID().toString();
+            LOGGER.debug("Correlation id not found in http header. Created a new one:::" + correlationId);
         }
+        //Maybe (not clear yet) needed for logging purposes, therefore set like in old VP
+        exchange.setProperty(VPExchangeProperties.SKLTP_CORRELATION_ID, correlationId);
         String isHttps = ((String)exchange.getProperty(VPExchangeProperties.IS_HTTPS));
         if (isHttps == null) {
             exchange.getIn().setHeader(HttpHeaders.X_SKLTP_CORRELATION_ID, correlationId);
@@ -40,8 +51,8 @@ public class HeaderConfigurationProcessorImpl implements HeaderConfigurationProc
             if (propagateCorrelationIdForHttps) {
                 exchange.getIn().setHeader(HttpHeaders.X_SKLTP_CORRELATION_ID, correlationId);
             }
-            exchange.removeProperty(VPExchangeProperties.IS_HTTPS);
         }
+        exchange.removeProperty(VPExchangeProperties.IS_HTTPS);
     }
 
     private void setOriginalConsumerId(Exchange exchange) {
@@ -52,6 +63,7 @@ public class HeaderConfigurationProcessorImpl implements HeaderConfigurationProc
 
         if (originalServiceconsumerHsaid != null && !originalServiceconsumerHsaid.trim().isEmpty()) {
             exchange.getIn().setHeader(HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID, originalServiceconsumerHsaid);
+            exchange.getIn().removeHeader(VPExchangeProperties.ORIGINAL_SERVICE_CONSUMER_HSA_ID);
         } else {
             exchange.getIn().setHeader(HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID, senderId);
         }
