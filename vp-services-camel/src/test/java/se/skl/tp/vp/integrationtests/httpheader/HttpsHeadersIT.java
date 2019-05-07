b015d4f67b708f79efc3909926928d0cb6640d0c
hbragman <hans.bragman@soprasteria.com>
2019-05-07 11:42:46 +0200

    Using boolean instead of string.

--- vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/httpheader/HttpsHeadersIT.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/httpheader/HttpsHeadersIT.java
@@ -1,6 +1,10 @@
 package se.skl.tp.vp.integrationtests.httpheader;
 
-import org.apache.camel.*;
+import java.io.IOException;
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit4.CamelTestSupport;
@@ -11,28 +15,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
 import se.skl.tp.vp.TestBeanConfiguration;
 import se.skl.tp.vp.constants.PropertyConstants;
 import se.skl.tp.vp.httpheader.HeaderConfigurationProcessorImpl;
 import se.skl.tp.vp.integrationtests.utils.TakMockWebService;
 import se.skl.tp.vp.util.soaprequests.TestSoapRequests;
 
-import java.io.IOException;
-
 import static se.skl.tp.vp.constants.HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID;
 import static se.skl.tp.vp.constants.HttpHeaders.X_SKLTP_CORRELATION_ID;
 
 @RunWith(CamelSpringBootRunner.class)
 @SpringBootTest(classes = TestBeanConfiguration.class)
-@TestPropertySource("classpath:application.properties")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class HttpsHeadersIT extends CamelTestSupport {
 
     public static TakMockWebService takMockWebService;
 
     @Value("${" + PropertyConstants.PROPAGATE_CORRELATION_ID_FOR_HTTPS + "}")
-    String propagate;
+    private Boolean propagateCorrIdForHttps;
 
     @EndpointInject(uri = "mock:result")
     protected MockEndpoint resultEndpoint;
@@ -47,10 +47,14 @@ public class HttpsHeadersIT extends CamelTestSupport {
         takMockWebService.start();
     }
 
+    @AfterClass
+    public static void afterClass() {
+        takMockWebService.stop();
+    }
     @Autowired
     private HeaderConfigurationProcessorImpl headerConfigurationProcessor;
 
-    private String oldCorrelation;
+    private boolean oldCorrelation;
 
     @Autowired
     private CamelContext camelContext;
@@ -75,41 +79,37 @@ public class HttpsHeadersIT extends CamelTestSupport {
 
     @Test
     public void setCorrelationAndConsumerIdNoMembersPassCorrolationTrueTest() {
-        headerConfigurationProcessor.setPropagate("true");
+        headerConfigurationProcessor.setPropagate(true);
         template.sendBodyAndHeaders(TestSoapRequests.GET_CERT_HTTPS_REQUEST, HeadersUtil.getHttpsHeadersWithoutMembers());
         assert(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID).equals("tp"));
         String s = (String) resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_SKLTP_CORRELATION_ID);
         assertNotNull(s);
         assert(!s.equals("aTestCorrelationId"));
         assert(s.length() > 20);
-        takMockWebService.stop();
     }
 
     @Test
     public void setCorrelationAndConsumerIdNoMembersPassCorrolationFalseTest() throws Exception {
-        headerConfigurationProcessor.setPropagate("false");
+        headerConfigurationProcessor.setPropagate(false);
         template.sendBodyAndHeaders(TestSoapRequests.GET_CERT_HTTPS_REQUEST, HeadersUtil.getHttpsHeadersWithoutMembers());
         assert(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID).equals("tp"));
         assertNull(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_SKLTP_CORRELATION_ID));
-        takMockWebService.stop();
     }
 
     @Test
     public void setCorrelationAndConsumerIdPassCorrolationTrueTest() throws Exception {
-        headerConfigurationProcessor.setPropagate("true");
+        headerConfigurationProcessor.setPropagate(true);
         template.sendBodyAndHeaders(TestSoapRequests.GET_CERT_HTTPS_REQUEST, HeadersUtil.getHttpsHeadersWithMembers());
         assert(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID).equals("aTestConsumer"));
         assert(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_SKLTP_CORRELATION_ID).equals("aTestCorrelationId"));
-        takMockWebService.stop();
     }
 
     @Test
     public void setCorrelationAndConsumerIdPassCorrolationFalseTest() throws Exception {
-        headerConfigurationProcessor.setPropagate("false");
+        headerConfigurationProcessor.setPropagate(false);
         template.sendBodyAndHeaders(TestSoapRequests.GET_CERT_HTTPS_REQUEST, HeadersUtil.getHttpsHeadersWithMembers());
         assert(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID).equals("aTestConsumer"));
         assertNull(resultEndpoint.getExchanges().get(0).getIn().getHeaders().get(X_SKLTP_CORRELATION_ID));
-        takMockWebService.stop();
     }
 
     private void addConsumerRoute(CamelContext camelContext) throws Exception {
@@ -120,6 +120,7 @@ public class HttpsHeadersIT extends CamelTestSupport {
             from("direct:start").routeId("start")
                 .to("netty4-http:https://localhost:1024/vp?sslContextParameters=#incomingSSLContextParameters&ssl=true&" +
                         "sslClientCertHeaders=true&needClientAuth=true&matchOnUriPrefix=true");
+
             from("netty4-http:https://localhost:19000/vardgivare-b/tjanst2?sslContextParameters=#outgoingSSLContextParameters&ssl=true")
                 .to("mock:result");
           }
