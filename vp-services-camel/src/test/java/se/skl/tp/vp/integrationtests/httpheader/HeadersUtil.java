0dc24a0838b859de938a5978f590664ec2a2c29b
hbragman <hans.bragman@soprasteria.com>
2019-05-08 10:08:32 +0200

    Added user-agent and content-type to test headers.

--- vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/httpheader/HeadersUtil.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/httpheader/HeadersUtil.java
@@ -2,17 +2,16 @@ package se.skl.tp.vp.integrationtests.httpheader;
 
 import se.skl.tp.vp.constants.HttpHeaders;
 import se.skl.tp.vp.constants.VPExchangeProperties;
-import se.skl.tp.vp.util.soaprequests.TestSoapRequests;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.apache.camel.language.constant.ConstantLanguage.constant;
 
 public class HeadersUtil {
 
     public static Map getHttpsHeadersWithoutMembers() {
         Map<String, Object> headers = new HashMap<>();
+        headers.put(HttpHeaders.HEADER_CONTENT_TYPE, "text/xml; charset=UTF-8");
+        headers.put(HttpHeaders.HEADER_USER_AGENT, "SKLTP VP/3.1");
         return headers;
     }
 
@@ -20,7 +19,8 @@ public class HeadersUtil {
         Map<String, Object> headers = new HashMap<>();
         headers.put(HttpHeaders.X_VP_SENDER_ID, "tp");
         headers.put(HttpHeaders.X_VP_INSTANCE_ID, "dev_env");
-        headers.put("X-Forwarded-For", constant("1.2.3.4"));
+        headers.put(HttpHeaders.HEADER_CONTENT_TYPE, "text/xml; charset=UTF-8");
+        headers.put(HttpHeaders.HEADER_USER_AGENT, "SKLTP VP/3.1");
         return headers;
     }
 
