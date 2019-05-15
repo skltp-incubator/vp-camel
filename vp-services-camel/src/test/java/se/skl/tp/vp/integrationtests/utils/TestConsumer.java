cfea0fb90ce5a32a2fc0746619fb96148431254b
martul <mariia.tuliakova@soprasteria.com>
2019-05-15 14:36:34 +0200

    lösning av camel problem med url "optimering"

--- vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/utils/TestConsumer.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/utils/TestConsumer.java
@@ -49,8 +49,8 @@ public class TestConsumer {
   private void createConsumerRouteUrls(){
     String vpHttpBaseUrl = env.getProperty(PropertyConstants.VP_HTTP_ROUTE_URL);
     String vpHttpsBaseUrl = env.getProperty(PropertyConstants.VP_HTTPS_ROUTE_URL);
-    httpConsumerRouteUrl = NETTY_PREFIX + vpHttpBaseUrl + "?throwExceptionOnFailure=false";
-    httpsConsumerRouteUrl = NETTY_PREFIX + vpHttpsBaseUrl + "?sslContextParameters=#outgoingSSLContextParameters&ssl=true&throwExceptionOnFailure=false";
+    httpConsumerRouteUrl = NETTY_PREFIX + vpHttpBaseUrl + "/PATH1 "+ "?throwExceptionOnFailure=false";
+    httpsConsumerRouteUrl = NETTY_PREFIX + vpHttpsBaseUrl + "/PATH1 " + "?sslContextParameters=#outgoingSSLContextParameters&ssl=true&throwExceptionOnFailure=false";
   }
 
   private void createConsumerRoutes(CamelContext camelContext) throws Exception {
