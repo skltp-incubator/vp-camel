ba49e59211cc177ee0456fda05b62603dd8a5692
martul <mariia.tuliakova@soprasteria.com>
2019-05-20 15:54:49 +0200

    ändringar i felmedelande

--- vp-services-camel/src/main/java/se/skl/tp/vp/vagval/BehorighetProcessor.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/vagval/BehorighetProcessor.java
@@ -35,7 +35,7 @@ public class BehorighetProcessor implements Processor {
         boolean isAuthorized = takService.isAuthorized(senderId, servicecontractNamespace, receiverId);
         exchange.setProperty(VPExchangeProperties.ANROPSBEHORIGHET_TRACE, ThreadContextLogTrace.get(ThreadContextLogTrace.ROUTER_RESOLVE_ANROPSBEHORIGHET_TRACE) );
         if( !isAuthorized ){
-            throw exceptionUtil.createVpSemanticException( VpSemanticErrorCodeEnum.VP007, "Not authorized call, " + getRequestSummaryString(senderId, servicecontractNamespace, receiverId));
+            throw exceptionUtil.createVpSemanticException( VpSemanticErrorCodeEnum.VP007, getRequestSummaryString(servicecontractNamespace, receiverId, senderId));
         }
     }
 
@@ -44,18 +44,18 @@ public class BehorighetProcessor implements Processor {
 
         // No sender ID (from_address) found in certificate
         if(senderId == null){
-            throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.VP002, getRequestSummaryString(senderId, servicecontractNamespace, receiverId));
+            throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.VP002, getRequestSummaryString(servicecontractNamespace, receiverId, senderId));
         }
 
         // No receiver ID (to_address) found in message
         if(receiverId == null){
-            throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.VP003, getRequestSummaryString(senderId, servicecontractNamespace, receiverId));
+            throw exceptionUtil.createVpSemanticException(VpSemanticErrorCodeEnum.VP003, getRequestSummaryString(servicecontractNamespace, receiverId, senderId));
         }
 
     }
 
-    private String getRequestSummaryString(String senderId, String serviceNamespace, String logicalAddress) {
-        return String.format( "SenderId: %s serviceNamespace: %s, logicalAddress: %s",senderId, serviceNamespace, logicalAddress);
+    private String getRequestSummaryString(String serviceNamespace, String logicalAddress, String senderId) {
+        return String.format( "serviceNamespace: %s, receiverId: %s, senderId: %s", serviceNamespace, logicalAddress, senderId);
     }
 
 }
