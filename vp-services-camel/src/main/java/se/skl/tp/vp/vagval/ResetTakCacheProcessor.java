69efb8d6ea30d8615c84cfd65e4af055a30fdc06
EMEAAD\gerkstam <goran.erkstam@soprasteria.com>
2019-05-23 08:42:37 +0200

    NTP-825: Add status of HsaCache and cleanup

--- vp-services-camel/src/main/java/se/skl/tp/vp/vagval/ResetTakCacheProcessor.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/vagval/ResetTakCacheProcessor.java
@@ -1,21 +1,14 @@
 package se.skl.tp.vp.vagval;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import se.skl.tp.vp.service.TakCacheService;
 import se.skltp.takcache.TakCacheLog;
 
-import java.util.Date;
-import java.util.Map;
-
 @Component
 public class ResetTakCacheProcessor implements Processor {
-    private Logger log = LoggerFactory.getLogger(ResetTakCacheProcessor.class);
 
     private final TakCacheService takService;
 
@@ -24,13 +17,8 @@ public class ResetTakCacheProcessor implements Processor {
         this.takService = takService;
     }
 
-    /*@Autowired
-    private CamelContext camelContext;*/
-
     @Override
     public void process(Exchange exchange) throws Exception {
-       /* Map map = camelContext.getGlobalOptions();
-        map.put("TAK_CACHE_RESET", new Date().toString());*/
         TakCacheLog result = takService.refresh();
         exchange.getOut().setBody(getResultAsString(result));
         exchange.getOut().setHeader("Content-Type", "text/html;");
