72fcc94cea15f311936862169aa4ff203ed69212
hbragman <hans.bragman@soprasteria.com>
2019-05-28 15:54:43 +0200

    Skipped static strings.

--- vp-services-camel/src/main/java/se/skl/tp/vp/httpheader/CheckSenderAllowedToUseHeaderImpl.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/httpheader/CheckSenderAllowedToUseHeaderImpl.java
@@ -19,21 +19,22 @@ public class CheckSenderAllowedToUseHeaderImpl implements CheckSenderAllowedToUs
 
   private static Logger LOGGER = LogManager.getLogger(CheckSenderAllowedToUseHeaderImpl.class);
   private String[] senderIdArray;
+  private String senderIdString;
 
-  public static String SENDER_ID_NULL = "The sender was null/empty. Could not check address in list {}. HTTP header that caused checking: {}.";
-  public static String LIST_NULL = "{} was NULL, so nothing to compare sender {} against. HTTP header that caused checking: {}.";
-  public static String LIST_EMPTY = "A check for sender {} against the ip address in {} was requested, but the list is configured empty.\n"
+  private String senderIdNull = "The sender was null/empty. Could not check address in list {}. HTTP header that caused checking: {}.";
+  private String listNull = "{} was NULL, so nothing to compare sender {} against. HTTP header that caused checking: {}.";
+  private String listEmpty = "A check for sender {} against the ip address in {} was requested, but the list is configured empty.\n"
           + "Update the list OR set flag sender.id.check.enforce to false.\n" +
           "HTTP header that caused checking: {}.";
-  public static String SENDER_ID_ON_LIST = "Caller {} matches ip address/subdomain in {}";
-  public static String SENDER_ID_NOT_ON_LIST = "SenderId was not on the list {}. SenderId: {}, accepted senderId's in {}: <{}>";
+  private String senderIdOnList = "Caller {} matches ip address/subdomain in {}";
+  private String senderIdNotOnList = "SenderId was not on the list {}. SenderId: {}, accepted senderId's in {}: <{}>";
 
   @Autowired
-  public CheckSenderAllowedToUseHeaderImpl(
-      @Value("${" + PropertyConstants.SENDER_ID_ALLOWED_LIST + ":#{null}}") String consumerlistString) {
+  public CheckSenderAllowedToUseHeaderImpl(@Value("${" + PropertyConstants.SENDER_ID_ALLOWED_LIST + ":#{null}}") String senderAllowedList) {
 
-    if (!StringUtils.isEmpty(consumerlistString)) {
-      senderIdArray = consumerlistString.split(",");
+    if (!StringUtils.isEmpty(senderAllowedList)) {
+      senderIdArray = senderAllowedList.split(",");
+      senderIdString = senderAllowedList;
     }
   }
 
@@ -44,33 +45,27 @@ public class CheckSenderAllowedToUseHeaderImpl implements CheckSenderAllowedToUs
   public boolean isSenderIdAllowedToUseXrivtaOriginalConsumerIdHeader(String senderId) {
 
     if (senderId == null || senderId.trim().isEmpty()) {
-      LOGGER.warn(SENDER_ID_NULL, PropertyConstants.SENDER_ID_ALLOWED_LIST, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
+      LOGGER.warn(senderIdNull, PropertyConstants.SENDER_ID_ALLOWED_LIST, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
       return false;
     }
 
     if (senderIdArray == null) {
-      LOGGER.warn(LIST_NULL, PropertyConstants.SENDER_ID_ALLOWED_LIST, senderId, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
+      LOGGER.warn(listNull, PropertyConstants.SENDER_ID_ALLOWED_LIST, senderId, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
       return false;
     }
 
     if (senderIdArray.length == 0) {
-      LOGGER.warn(LIST_EMPTY, senderId, PropertyConstants.SENDER_ID_ALLOWED_LIST, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
+      LOGGER.warn(listEmpty, senderId, PropertyConstants.SENDER_ID_ALLOWED_LIST, HttpHeaders.X_RIVTA_ORIGINAL_SERVICE_CONSUMER_HSA_ID);
       return false;
     }
 
     for (String ipAddress : senderIdArray) {
       if (senderId.trim().startsWith(ipAddress.trim())) {
-        LOGGER.info(SENDER_ID_ON_LIST, senderId, PropertyConstants.SENDER_ID_ALLOWED_LIST);
+        LOGGER.info(senderIdOnList, senderId, PropertyConstants.SENDER_ID_ALLOWED_LIST);
         return true;
       }
     }
-    StringBuilder content = new StringBuilder();
-    for (String s : senderIdArray) {
-      content.append(s + ",");
-    }
-    content.deleteCharAt(content.length() - 1);
-    LOGGER.warn(SENDER_ID_NOT_ON_LIST, PropertyConstants.SENDER_ID_ALLOWED_LIST, senderId,
-            PropertyConstants.SENDER_ID_ALLOWED_LIST, content.toString());
+    LOGGER.warn(senderIdNotOnList, PropertyConstants.SENDER_ID_ALLOWED_LIST, senderId, PropertyConstants.SENDER_ID_ALLOWED_LIST, senderIdString);
     return false;
   }
 }
