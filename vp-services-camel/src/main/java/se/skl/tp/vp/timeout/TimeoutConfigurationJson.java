da34b391e1e2ac45597ba4b3c3375625bb73c771
hbragman <hans.bragman@soprasteria.com>
2019-06-19 16:11:04 +0200

    Formatting

--- vp-services-camel/src/main/java/se/skl/tp/vp/timeout/TimeoutConfigurationJson.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/timeout/TimeoutConfigurationJson.java
@@ -19,60 +19,64 @@ import java.util.List;
 @Service
 public class TimeoutConfigurationJson implements TimeoutConfiguration {
 
-    private static Logger LOGGER = LogManager.getLogger(TimeoutConfigurationJson.class);
+  private static Logger LOGGER = LogManager.getLogger(TimeoutConfigurationJson.class);
 
-    private List<TimeoutConfig> wsdlConfigs;
-    private HashMap<String, TimeoutConfig> mapOnTjanstekontrakt;
+  private List<TimeoutConfig> wsdlConfigs;
+  private HashMap<String, TimeoutConfig> mapOnTjanstekontrakt;
 
-    public void setMapOnTjansteKontrakt(HashMap<String, TimeoutConfig> map) {
-        //Used in tests
-        mapOnTjanstekontrakt = map;
+  public void setMapOnTjansteKontrakt(HashMap<String, TimeoutConfig> map) {
+    // Used in tests
+    mapOnTjanstekontrakt = map;
+  }
+
+  public TimeoutConfigurationJson(
+      @Value("${" + PropertyConstants.TIMEOUT_JSON_FILE + "}") String timeout_json_file,
+      @Value("${" + PropertyConstants.TIMEOUT_JSON_FILE_DEFAULT_TJANSTEKONTRAKT_NAME + "}")
+          String timeout_json_file_default_tjanstekontrakt_name)
+      throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      wsdlConfigs = objectMapper.readValue(new File(timeout_json_file), new TypeReference<List<TimeoutConfig>>() {});
+    } catch (FileNotFoundException e) {
+      LOGGER.warn("Json file for timeouts not found at " + timeout_json_file + ".");
+    } catch (JsonParseException e) {
+      LOGGER.warn("Json file for timeouts " + timeout_json_file + " could not be parsed.");
+    }
+    if (wsdlConfigs == null) {
+      wsdlConfigs = new ArrayList<>();
+    }
+    boolean defaultTimeoutsExist = false;
+    for (TimeoutConfig timeoutConfig : wsdlConfigs) {
+      if (timeoutConfig.getTjanstekontrakt().equalsIgnoreCase(timeout_json_file_default_tjanstekontrakt_name)) {
+        defaultTimeoutsExist = true;
+      }
+    }
+    if (!defaultTimeoutsExist) {
+      createDefaultTimeoutsWhenMissing(timeout_json_file_default_tjanstekontrakt_name);
+      LOGGER.warn("Could not find any default timeoutvalues, using producertimeout=29000 and routetimeout=30000 as default timeouts. " +
+              "Please create and configure a timeoutconfig.json file to set this manually.");
     }
 
-    public TimeoutConfigurationJson(@Value("${" + PropertyConstants.TIMEOUT_JSON_FILE + "}") String timeout_json_file,
-                                    @Value("${" + PropertyConstants.TIMEOUT_JSON_FILE_DEFAULT_TJANSTEKONTRAKT_NAME + "}") String timeout_json_file_default_tjanstekontrakt_name) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try{
-            wsdlConfigs = objectMapper.readValue(new File(timeout_json_file), new TypeReference<List<TimeoutConfig>>(){});
-        } catch(FileNotFoundException e) {
-            LOGGER.warn("Json file for timeouts not found at "+ timeout_json_file +".");
-        } catch(JsonParseException e) {
-            LOGGER.warn("Json file for timeouts "+ timeout_json_file +" could not be parsed.");
-        }
-        if(wsdlConfigs == null) {
-            wsdlConfigs = new ArrayList<>();
-        }
-        boolean defaultTimeoutsExist= false;
-        for (TimeoutConfig timeoutConfig : wsdlConfigs) {
-             if(timeoutConfig.getTjanstekontrakt().equalsIgnoreCase(timeout_json_file_default_tjanstekontrakt_name)){
-                 defaultTimeoutsExist=true;
-             }
-        }
-        if(!defaultTimeoutsExist){
-            createDefaultTimeoutsWhenMissing(timeout_json_file_default_tjanstekontrakt_name);
-            LOGGER.warn("Could not find any default timeoutvalues, using producertimeout=29000 and routetimeout=30000 as default timeouts. Please create and configure a timeoutconfig.json file to set this manually.");
-        }
+    initMaps();
+  }
 
-        initMaps();
-    }
+  private void createDefaultTimeoutsWhenMissing(String defaultTjanstekontrakt) {
+    TimeoutConfig timeoutConfig = new TimeoutConfig();
+    timeoutConfig.setTjanstekontrakt(defaultTjanstekontrakt);
+    timeoutConfig.setProducertimeout(29000);
+    timeoutConfig.setRoutetimeout(30000);
+    wsdlConfigs.add(timeoutConfig);
+  }
 
-    private void createDefaultTimeoutsWhenMissing(String defaultTjanstekontrakt) {
-        TimeoutConfig timeoutConfig = new TimeoutConfig();
-        timeoutConfig.setTjanstekontrakt(defaultTjanstekontrakt);
-        timeoutConfig.setProducertimeout(29000);
-        timeoutConfig.setRoutetimeout(30000);
-        wsdlConfigs.add(timeoutConfig);
+  private void initMaps() {
+    mapOnTjanstekontrakt = new HashMap<>();
+    for (TimeoutConfig timeoutConfig : wsdlConfigs) {
+      mapOnTjanstekontrakt.put(timeoutConfig.getTjanstekontrakt(), timeoutConfig);
     }
+  }
 
-    private void initMaps() {
-        mapOnTjanstekontrakt = new HashMap<>();
-        for (TimeoutConfig timeoutConfig : wsdlConfigs) {
-            mapOnTjanstekontrakt.put(timeoutConfig.getTjanstekontrakt(), timeoutConfig);
-        }
-    }
-
-    @Override
-    public TimeoutConfig getOnTjanstekontrakt(String tjanstekontrakt) {
-        return mapOnTjanstekontrakt.get(tjanstekontrakt);
-    }
+  @Override
+  public TimeoutConfig getOnTjanstekontrakt(String tjanstekontrakt) {
+    return mapOnTjanstekontrakt.get(tjanstekontrakt);
+  }
 }
