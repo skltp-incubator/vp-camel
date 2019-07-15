c048d56198fd6b17535188553d59b884133ef9a1
hbragman <hans.bragman@soprasteria.com>
2019-07-15 15:49:06 +0200

    Working test for respCode 500 with null in req. Duplicate from ErrorInResponse.java?

--- vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/errorhandling/CheckCode500InResponseIT.java
+++ vp-services-camel/src/test/java/se/skl/tp/vp/integrationtests/errorhandling/CheckCode500InResponseIT.java
@@ -25,6 +25,7 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static se.skl.tp.vp.VPRouter.DIRECT_PRODUCER_ROUTE;
+import static se.skl.tp.vp.util.soaprequests.TestSoapRequests.RECEIVER_RIV20;
 
 @RunWith(CamelSpringBootRunner.class)
 @SpringBootTest(classes = TestBeanConfiguration.class)
@@ -64,24 +65,11 @@ public class CheckCode500InResponseIT {
 
   @Test
   public void checkCode500InHttpResponseTest() {
-    String body = TestSoapRequests.GET_NO_CERT_HTTP_SOAP_REQUEST_NO_VAGVAL_RECEIVER;
+    String body = TestSoapRequests.createGetActivitiesRiv20Request(RECEIVER_RIV20);
     Map headers = HeadersUtil.getHttpHeadersWithoutMembers();
-
+    body = body.replace("<urn2:extension>197404188888</urn2:extension>", "<urn2:extension></urn2:extension>");
     try {
-      addConsumerRoute(camelContext);
-      template.sendBodyAndHeaders(body, headers);
-    } catch (Exception e) {
-      assertExceptionContent(e);
-    }
-  }
-
-  @Test
-  public void checkCode500InHttpsResponseTest() {
-    String body = TestSoapRequests.GET_CERT_HTTPS_REQUEST;
-    Map headers = HeadersUtil.getHttpsHeadersWithoutMembers();
-
-    try {
-      addHttpsConsumerRoute(camelContext);
+      addHttpConsumerRoute(camelContext);
       template.sendBodyAndHeaders(body, headers);
     } catch (Exception e) {
       assertExceptionContent(e);
@@ -98,25 +86,12 @@ public class CheckCode500InResponseIT {
       code = n.getStatusCode();
     }
     assertEquals(500, code);
-    assertTrue(content.contains("VP004 No receiverId (logical address) found for serviceNamespace: " +
-            "urn:riv:insuranceprocess:healthreporting:GetCertificateResponder:1, receiverId: NoVagvalReceiver"));
+    assertTrue(content.contains("<faultcode>soap:Server</faultcode>"));
+    assertTrue(content.contains("<faultstring>null</faultstring>"));
   }
 
-  private void addHttpsConsumerRoute(CamelContext camelContext) throws Exception {
-    camelContext.addRoutes(
-            new RouteBuilder() {
-              @Override
-              public void configure() {from("direct:start").routeId("start")
-                .to("netty4-http:" + httpsRoute + "?sslContextParameters=#incomingSSLContextParameters&ssl=true&"
-                  + "sslClientCertHeaders=true&needClientAuth=true&matchOnUriPrefix=true");
-                // Address below from tak-vagval-test.xml
-                from("netty4-http:https://localhost:19001/vardgivare-b/tjanst2?sslContextParameters=#outgoingSSLContextParameters&ssl=true")
-                .to("mock:result");
-              }
-            });
-  }
 
-  private void addConsumerRoute(CamelContext camelContext) throws Exception {
+  private void addHttpConsumerRoute(CamelContext camelContext) throws Exception {
     camelContext.addRoutes(
             new RouteBuilder() {
               @Override
@@ -124,7 +99,7 @@ public class CheckCode500InResponseIT {
                 from("direct:start").routeId("start").routeDescription("consumer")
                 .to("netty4-http:" + httpRoute);
                 // Address below from tak-vagval-test.xml
-                from("netty4-http:http://localhost:19000/vardgivare-b/tjanst2").routeDescription("producer").to(DIRECT_PRODUCER_ROUTE)
+                from("netty4-http:http://localhost:19000/GetActivitiesResponder").routeDescription("producer").to(DIRECT_PRODUCER_ROUTE)
                 .to("mock:result");
               }
             });
