af1aef47bc814796d71a923763f72e74dcde27f1
Hans Bragman <hans.bragman@soprasteria.com>
2019-11-04 09:17:07 +0100

    NTP-983 Better method handling.

--- vp-services-camel/src/main/java/se/skl/tp/vp/errorhandling/SoapFaultHelper.java
+++ vp-services-camel/src/main/java/se/skl/tp/vp/errorhandling/SoapFaultHelper.java
@@ -74,15 +74,16 @@ public class SoapFaultHelper {
 
   public static void setSoapFaultInResponse(Exchange exchange, String cause, String errorCode){
     exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 500);
-    setSessionParameters(exchange, errorCode);
+    setSessionParameters(exchange, cause, errorCode);
   }
 
   public static void setSoapFaultInResponseWithRespCode200(Exchange exchange, String cause, String errorcode) {
     exchange.getOut().setHeader(Exchange.HTTP_RESPONSE_CODE, 200);
-    setSessionParameters(exchange, errorcode);
+    setSessionParameters(exchange, cause, errorcode);
   }
 
-  public static void setSessionParameters(Exchange exchange, String errorCode) {
+  public static void setSessionParameters(Exchange exchange, String cause, String errorCode) {
+    exchange.getOut().setBody(createSoapFault(cause));
     exchange.setProperty(VPExchangeProperties.SESSION_ERROR, Boolean.TRUE);
     exchange.setProperty(VPExchangeProperties.SESSION_ERROR_CODE, errorCode);
     exchange.setProperty(VPExchangeProperties.SESSION_HTML_STATUS, SoapFaultHelper.getStatusMessage(nvl(exchange.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE)), null));
